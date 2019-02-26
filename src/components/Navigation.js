bfbd312e94c078528b99d43b9fc820263df94843
Mario Rizki <mariorizki07@gmail.com>
2019-02-27 01:36:27 +0700

    implement navigation styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,25 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import { Link } from 'react-router-dom';
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+  background-color: #000;
+`;
+
+const NavUl = styled.ul`
+  display: flex;
+  align-items: center;
+`;
+
+const NavLi = styled.li`
+  display: inline;
+  font-weight: bolder;
+  margin: 0 20px;
+  color: #fff;
+`;
+
+const Logo = styled.img`
+  height: 80px;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #fff;
+`;
 
 const Navigation = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/">Logo</Link>
-          </li>
-          <li>Register</li>
-          <li>Login</li>
-        </ul>
-      </nav>
-    </header>
+    <Nav>
+      <NavUl>
+        <NavLi>
+          <StyledLink to="/">Home</StyledLink>
+        </NavLi>
+        <NavLi>
+          <StyledLink to="/about">About</StyledLink>
+        </NavLi>
+        <NavLi>
+          <StyledLink to="/">
+            <Logo src="/assets/ideabyte-logo-white.svg" alt="Idea Byte Logo" />
+          </StyledLink>
+        </NavLi>
+        <NavLi>Register</NavLi>
+        <NavLi>Login</NavLi>
+      </NavUl>
+    </Nav>
   );
 };
 
